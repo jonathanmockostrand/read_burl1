a03c47c4c1f671d1db89542bfed878d624c9129a
jonathanmockostrand <obitustrand@gmail.com>
2013-09-12 13:49:37 -0500

    Update burl1_2011_Class.py

--- burl1_2011_Class.py
+++ burl1_2011_Class.py
@@ -13,60 +13,58 @@
 #http://www.ndbc.noaa.gov/download_data.php?filename=burl1h2011.txt.gz&dir=data/historical/stdmet/
 #
 ################################################################################
-#
-#Function:
-#    noaa_convert(file)
-#
-#Input:
-#    file.dat
-#    
-#Output:
-#    Date as datetime
-#    Pressure as sea-level pressure
-#    wU and wV as Components U and V of wind vector 
-#    (converted from wind speed and direction)
-#
-################################################################################
 
 import numpy as np
 import datetime as dt
 
 class burl1_2011():
+    '''
+    Function:
+        noaa_convert(file)
+
+    Input:
+        file.dat
+        
+    Output:
+        Date as datetime
+        Pressure as sea-level pressure
+        wU and wV as Components U and V of wind vector 
+        (converted from wind speed and direction)
+    '''
     
     def __init__(self, datafile):
-        self.datafile = datafile
-
-    f = open('burl1_2011.dat')
-
-    date = []
-    pressure = []
-    wU = []
-    wV = []
-
-    for line in f.readlines()[2:]:
-        data = line.split()
-        YYYY = int(data[0])
-        MM = int(data[1])
-        DD = int(data[2])
-        hh = int(data[3])
-        mm = int(data[4])
-    
-        date.append( dt.datetime(YYYY, MM, DD, hh, mm) )
-        pressure.append( float(data[12]) )
-        wU.append( -(float(data[6])) * np.sin(float(data[5]) * np.pi/180) )
-        wV.append( -(float(data[6])) * np.cos(float(data[5]) * np.pi/180) )
-
-
-    date = np.array(date)
-    pressure = np.array(pressure)
-    wU = np.array(wU)
-    wV = np.array(wV)
-
-    print '#Dates     \n', date
-    print '#Pressure     \n', pressure
-    print '#wU     \n', wU
-    print '#wV     \n', wV
-
         
-#burl1_2011('burl1_2011.dat')
-#print output
+        self.datafile = datafile
+        f = open(self.datafile)
+
+        date = []
+        pressure = []
+        wspeed = []
+        wdirection = []
+
+        for line in f.readlines()[2:]:
+            data = line.split()
+            YYYY = int(data[0])
+            MM = int(data[1])
+            DD = int(data[2])
+            hh = int(data[3])
+            mm = int(data[4])
+    
+            date.append( dt.datetime(YYYY, MM, DD, hh, mm) )
+            pressure.append( float(data[12]) )
+            wspeed.append ( float(data[6]) )
+            wdirection.append ( float(data[5]) )
+
+        date = np.array(date)
+        pressure = np.array(pressure)
+        wspeed = np.array(wspeed)
+        wdirection = np.array(wdirection) 
+        U = np.array( -wspeed * np.sin(wdirection * np.pi/180) )
+        V = np.array( -wspeed * np.cos(wdirection * np.pi/180) )
+
+        self.date=date
+        self.pressure=pressure
+        self.U=U
+        self.V=V
+        
+d11 = burl1_2011('burl1_2011.dat')
